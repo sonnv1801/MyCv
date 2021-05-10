ad8f8a17efd6abf3e40c2b68ee2aa8045782deac
Your Name <you@example.com>
2021-05-10 12:02:18 +0700

    MyCv

---
+++ MyCv.js
@@ -0,0 +1,12 @@
+function MyCv(){
+    alert("Họ Và Tên: Nguyễn Văn Sơn\nNgày Sinh: 18/10/2001\nĐịa Chỉ: Nghĩa Trung, Tư Nghĩa, Quảng Ngãi\nGiới Tính: Nam");
+}
+function MyZalo(){
+    alert("Zalo: Nguyễn Văn Sơn\nTài Khoản: 0375521434");
+}
+function MyPhone(){
+    alert("Số Điện Thoại: 0375521434\nLiên Hệ Cho Chúng Tôi")
+}
+function MyEmail(){
+    alert("Địa Chỉ Email:\nnguyenvanson18102001@gmail.com")
+}
